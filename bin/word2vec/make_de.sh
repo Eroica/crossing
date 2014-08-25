f16f66e7ef8bde30401913a713d5f841b5a1d74d
Seba <lseba@icloud.com>
2014-08-25 19:29:49 +0200

    update setup.py

--- bin/word2vec/make_de.sh
+++ bin/word2vec/make_de.sh
@@ -3,4 +3,6 @@ make
 ./word2vec -train ../de.txt \
            -output ../de_vectors.txt \
            -min-count 0 \
-           -size 10
+           -size 10 \
+           -cbow 2 \
+           -window 5
