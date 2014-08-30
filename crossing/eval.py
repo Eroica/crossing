98f63d75a977c68567c70d59eb78992aaea7f1a7
Kaleidophon <Dennis.Ulmer@gmx.de>
2014-08-30 23:33:27 +0200

    Final commit

--- crossing/eval.py
+++ crossing/eval.py
@@ -27,14 +27,10 @@ import math
 import random
 
 import numpy as np
-from file_handling import readTupleFileToDict, readTupleFile, \
-	readVectorFileToDict, readDictionary, readVectorFile
+
+import FileManager
 import cPickle as pickle
 from scrn_out import w, wh, wil, fl, cl
-
-
-### import anpassen:
-# from vt import VectorTransformator
 import VectorManager
 
 
@@ -44,82 +40,47 @@ def main():
 	cl()
 	wh("\t\tCrOssinG: CompaRing Of AngliciSmS IN German", 75)
 
-	### nicht vergessen die pfade zu ändern
 	dictionary = readDictionary("../res/dictEntries.txt")
 	anglicisms = readTupleFile("../res/anglicisms.txt")
 	devectors = pickle.load(open("../res/DE_VEC.bin"))
 	envectors = pickle.load(open("../res/EN_VEC.bin"))
-	# devectors = readVectorFileToDict("../res/out/de_small2.txt")
-	# envectors = readVectorFileToDict("../res/out/en_small2.txt")
 	false_friends = readTupleFile("../res/false_friends.txt")
 
-	# alphas = [0.0001, 0.0002, 0.001, 0.002, 0.01, 0.02, 0.1, 0.2]
-	# models = ["ridge", "net", "Lasso"]
-	# model_paras = [(model, alpha) for model in models for alpha in alphas]
-	model_paras = [("net", 0.1), ("Lasso", 0.2)]
-	models = []
+	alphas = [0.0001, 0.0002, 0.001, 0.002, 0.01, 0.02, 0.1, 0.2]
+	models = ["ridge", "net", "Lasso"]
+	model_paras = [(model, alpha) for model in models for alpha in alphas]
 	i = 1
 	w("Creating VectorTransformators...\n")
 
-	vt = VectorManager.VectorTransformator()
-	vt.Dictionary = dictionary
-	vt.V = devectors
-	vt.W = envectors
+	vm = VectorManager.VectorTransformator()
+	vm.Dictionary = dictionary
+	vm.V = devectors
+	vm.W = envectors
 
 	for tuple_ in model_paras:
-		### transformations-matrix mit der methode createTransformationMatrix() erstellen
-		### die sieht so aus:
-		### def createTransformationMatrix(self, model="Lasso", alpha=0.1):
-		### also zuerst den namen des modells übergeben, dann den alphawert
-		### wörterbuch und vektoren müssen nicht übergeben werden, da sie im
-		### vt objekt enthalten sind.
-		### printErrors geht natürlich leider auch nicht mehr
-		vt.createTransformationMatrix(tuple_[0], tuple_[1])
-		
-		### wenn du auf die transformationsmatrizen zugreifen willst, mach das am besten so:
-		### vt[0].T
-		### über [0..] wird das 1., .. model von vt angesprochen. das modell selbst
-		### enthält die variable "T", die die matrix enthält
-		### vergiss nicht dass zu jedem modell nicht nur eine matrix gehört,
-		### sondern auch ein absolutes glied ...
-		### die folgende zeile könnte daher entfallen:
-		matrix = vt.M
-		
+		vm.createTransformationMatrix(tuple_[0], tuple_[1])
 		w("VectorTransformator Nr. %i with Model=%s and alpha=%g has been"
 		  " created\n" %(i, tuple_[0], tuple_[1]))
-		
-		### hier genauso: wenn du auf die einzelnen modelle zugreifen willst,
-		### benutz einfach vt[x].
-		### jedes dieser modelle hat auch die informationen zu modell und alpha gespeichert
-		### sagen wir vt[1] ist ein modell. dann hat es folgende variablen:
-		### vt[1].T :: matrix
-		### vt[1].b :: absolutes glied
-		### vt[1].model :: name des models (string)
-		### vt[1].alpha :: alpha-wert (float)
-		### grundsätzlich kannst du es aber auch so lassen
-		### später benutzt du ja die "models" variable weiter, also falls es zu kompliziert wird,
-		### lass es einfach so wie jetzt
-		### nur musst du bedenken, dass ich die erstellung von   vt   außerhalb
-		### der for-schleife gesetzt habe. es gibt also nur EIN vt objekt
-		### dieses vt objekt enthält, in der variablen vt.models, mehrere transformationsmatrizen
-		### wenn du diese transformationsmatrizen vergleichen willst, solltest du also
-		### vt.models an funktionen übergeben oder
-		### vt an sich und die funktionen greifen dann auf vt.models zu
-		models.append((tuple_[0], tuple_[1], vt, matrix))
-
 		i += 1
 
-
 	w("Creating VectorTransformators...Complete!\n\n")
+	models = vm.Models
+	top_model = compareMatrices(false_friends, vm, models, devectors, envectors)
 
-
-	### wie oben schon erwähnt: die "models" variable hier kann im prinzip ersetzt werden,
-	### indem einfach das    vt   objekt übergeben wird.
-	### vt hat ja auch die deutschen und englischen vektoren in vt.V und vt.W
-	top_model = compareMatrices(false_friends, models, devectors, envectors)
-	
-	falseFriendsCheck(false_friends, top_model[2] , devectors, envectors, dictionary, 50)
-
+	w("\nChecking the quality of an evaluation with false-friend-pairs...\n")
+	true_count = 0
+	false_count = 0
+	n_tests = 100
+	for i in range(n_tests):
+		wil("False friend test nr. %i" %(i+1))
+		res = falseFriendsCheck(false_friends, vm, top_model, devectors,\
+								envectors, dictionary, 50, False)
+		if res:
+			true_count += 1
+		elif not res:
+			false_count += 1
+	w("\nIn %i out of %i times, a random subset had a lower or equal average"\
+	 "similarity than a random false friend subset.\n" %(false_count, n_tests))
 
 def finalOutput(similarities, anglicisms, n):
 	""" Generates the "final output" of crossing, printing the most and least
@@ -146,7 +107,7 @@ def finalOutput(similarities, anglicisms, n):
 			w("%i. %s - %s | %.2f%% similarity\n"\
 			  %(bottom_n.index(pair)+1, pair[0][0], pair[0][1], pair[1]*50+50))
 
-def evalMatrix(false_friends, devectors, envectors, VectorTransformator,
+def evalMatrix(false_friends, devectors, envectors, vm, model,
 				output=True, n=5):
 	""" Evaluates the quality of a matrix """
 
@@ -158,7 +119,7 @@ def evalMatrix(false_friends, devectors, envectors, VectorTransformator,
 		try:
 			if devectors[pair[1]] == []: continue
 			elif envectors[pair[0]] == []: continue
-			mapped = mapVector(VectorTransformator, devectors[pair[1]])
+			mapped = mapVector(vm, model, devectors[pair[1]])
 			mapped = [x.item((0, 0)) for x in mapped]
 			sim = cosine_similarity(mapped, envectors[pair[0]])
 			similarities.append((sim, pair))
@@ -169,7 +130,6 @@ def evalMatrix(false_friends, devectors, envectors, VectorTransformator,
 	average_diff /= len(similarities)
 
 	# Output
-
 	if output:
 		print "Average similarity was %.2f%% for %i elements"\
 			  %(average_diff*50+50, len(similarities))
@@ -189,129 +149,100 @@ def evalMatrix(false_friends, devectors, envectors, VectorTransformator,
 	return average_diff*50+50
 
 
-def compareMatrices(false_friends, models, devectors, envectors): 
+def compareMatrices(false_friends, vm, models, devectors, envectors): 
 	""" Compares differend matrices and finds the best one """  
 
 	average_diffs = []
 
 	for model in models:
-		diff = evalMatrix(false_friends, devectors, envectors, model[2],\
-			output=True)
+		diff = evalMatrix(false_friends, devectors, envectors, vm, model,\
+			output=False)
 		average_diffs.append(diff)
 
 	top_average_diff = 200
-
 	for diff in average_diffs:
 		if diff <= top_average_diff: top_average_diff = diff
 
 	i = average_diffs.index(top_average_diff)+1
 	w("Matrix nr. %i is best by average similarity with %.2f%%\n"
-	  %(i+1, top_average_diff))
-	w("Parameters: Model=%s, alpha=%g\n\n" %(models[i-1][0], models[i-1][1]))
+	  %(i, top_average_diff))
+	w("Parameters: Model=%s, alpha=%g\n\n" 
+		%(models[i-1].model, models[i-1].alpha))
 
 	return models[i-1]
 
-def findSubstitutions(anglicism, dictionary, devectors, envectors, n=5,
-	printErrors=True):
-	""" Finds alternative words for an anglicism """
-
-	wil("Looking for words-substitutions for %s" %(anglicism))
-	translation = dictionary[anglicism]
-	anglicism_vector = devectors[anglicism]
-	translation_vector = envectors[translation]
-
-	length = len(devectors)
-	keys = devectors.keys()
-	proposals = []
-	errors = []
-
-	fl()
-	for i in xrange(length):
-		wil("Looking for words-substitutions for %s - %.2f%% complete%s" 
-			%(anglicism, i*1.0/length*100), 30)
-		try:
-			current_word = keys[i]
-			de_diff = vectorDistance(anglicism_vector, devectors[i])
-			en_diff = 0
-			# current_word_translation = lookUp(current_word, dictionary)
-			# en_diff = vectorDistance(translation_vector, 
-			# 	envectors[current_word_translation])
-			# if de_diff + en_diff != 0:
-			# 	# "Squared loss"
-			proposals.append(((de_diff + en_diff)**2, current_word))
-		except Exception, e:
-			errors.append(unicode(e))
-			continue
-		finally:
-			fl()
-
-	wil("Looking for words-substitutions for %s...Complete!%s\n" 
-		%(anglicism), 40)
-
-	# Sort ascending by difference
-	proposals = sorted(proposals, key =lambda e: e[0])
-
-
-	w("\nProposals for %s:\n" %(anglicism))
-	i = 1
-	for proposal in proposals[:n]:
-		w("%i. %s with a difference of %g\n" %(i, proposal[1], proposal[0]))
-		i += 1
-
-	if printErrors:
-		w("\nErrosr:\n")
-		for error in errors:
-			print error
-
-def falseFriendsCheck(false_friends, vectorTransformator, devectors, envectors,
-	dictionary, sample_size):
+def falseFriendsCheck(false_friends, vectorManager, model, devectors, envectors,
+	dictionary, sample_size, output=True):
 
 	ff_sim = 0
 	r_sim = 0
 
-	false_friends = randomSubset(false_friends, sample_size)
-	random_set = randomSubset(dictionary, sample_size)
+	false_friends = randomSubset(false_friends, sample_size, output)
+	random_set = randomSubset(dictionary, sample_size, output)
 
-	w("\nChecking the quality of an evaluation with false-friend-pairs\n")
+	if output:
+		w("\nChecking the quality of an evaluation with "\
+			"false-friend-pairs...\n")
 
+	# Calculates average similarity for random false friend subset
 	ff_count = 0
 	for pair in false_friends:
 		try:
 			vector_english = envectors[pair[0]]
 			vector_german = devectors[pair[1]]
 		except:
-			print "LookUp Fail"
 			continue
-		mapped = mapVector(VectorTransformator, vector_english)
+		mapped = mapVector(vectorManager, model, vector_english)
 		mapped = [x.item((0, 0)) for x in mapped]
 		ff_sim += dotproduct(mapped, vector_german)
 		ff_count += 1
 
 	ff_sim /= ff_count
-	print ff_sim
 
+	# Calculates similarity for random dictionary entry subset
+	r_count = 0
+	for pair in random_set:
+		if random_set.index(pair) > ff_count:
+			break # so both subsets have the same size and are comparable
+		try:
+			vector_english = envectors[pair[1]]
+			vector_german = devectors[pair[0]]
+		except:
+			continue
+		mapped = mapVector(vectorManager, model, vector_english)
+		mapped = [x.item((0, 0)) for x in mapped]
+		r_sim += dotproduct(mapped, vector_german)
+		r_count += 1
 
+	r_sim /= r_count 
 
-
-
+	if ff_sim < r_sim: 
+		return True
+	else:
+		return False
 
 #---------------------------- Helping functions -------------------------------
 
-def randomSubset(array, n):
-	w("Creating random subset...")
+def randomSubset(array, n, output=True):
+	if output:
+		w("Creating random subset...")
 	if isinstance(array, dict): 
+		# conversion to array of tuples
 		keys = array.keys()
 		values = array.values()
 		length = len(array)
 		array = [(keys[i], values[i]) for i in xrange(length)]
 	res = []
 	while len(res) != n:
-		percentage = len(res)*1.0/n*100
-		wil("Creating random subset - %.2f%% complete" %(percentage))
+		if output:
+			percentage = len(res)*1.0/n*100
+			wil("Creating random subset - %.2f%% complete" %(percentage))
 		ri = random.randint(0, len(array)-1)
 		res.append(array[ri])
-		fl()
-	wil("Creating random subset...Complete!", 50, "\n")
+		if output:
+			fl()
+	if output:		
+		wil("Creating random subset...Complete!", 50, "\n")
 	return res
 
 
@@ -322,8 +253,9 @@ def lookUp(key, dictionary):
 	except:
 		return None
 
-def mapVector(vt, v):
-	return vt.M * vt.prepareVector(v) + vt.b
+def mapVector(vm, model, v):
+	""" Maps a vector """
+	return model.T * vm.prepareVector(v) + model.b
 
 def findTopN(similarities, n, top=1):
 	""" Finds top n elements of an array """
@@ -346,16 +278,20 @@ def cosine_similarity(v1, v2):
 	return res
 
 def cosine_similarity2(v1, v2):
+	""" An alternative implementation of cosine similarity """
 	return dotproduct(v1, v2)/length(v1)/length(v2)
 
 def dotproduct(v1, v2):
-  return sum((a*b) for a, b in zip(v1, v2))
+	""" Returns the dotproduct of two vectors """
+ 	return sum((a*b) for a, b in zip(v1, v2))
 
 def length(v):
-  return math.sqrt(dotproduct(v, v))
+	""" Returns the length of a vector """
+ 	return math.sqrt(dotproduct(v, v))
 
 def angle(v1, v2):
-  return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
+	""" Returns the angle between two vectors """
+ 	return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
 
 def vectorDistance(v1, v2):
 	""" Returns the euklidian distance of two vectors """
@@ -402,8 +338,6 @@ def calculateSimilarities(devectors, envectors, dictionary, matrix,
 
 	return similarities
 
-
 #---------------------------------- Main --------------------------------------
-
 if __name__ == "__main__":
 	main()
